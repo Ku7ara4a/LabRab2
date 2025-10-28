8541147e22d296534718d638061e8ee1d391f939
Ku7ara4a <arxwoodj@gmail.com>
2025-10-28 18:00:39 +0300

    Обновленная логика и изменённое регулярное выражение

--- main.py
+++ main.py
@@ -1,51 +1,38 @@
 from re import *
 import requests
 
-DATE_FORMAT = r"\b[0-3][0-9]\.[0-1][0-9]\.\d{4}\b"
+DATE_FORMAT = compile(r"\b((?:0[1-9]|[12][0-9]|3[01])\.(?:0[1-9]|1[0-2])\.\d{4})\b")
+GIST_URL = "https://gist.github.com/Ku7ara4a/3a1d15d71eb5cbf314037f4999b45277/raw"
 
 def isCorrect(string: str) -> bool:
-    date = string.split('.')
-    day = int(date[0])
-    month = int(date[1])
-    year = int(date[2])
-    Leap = False
-    if day < 1 or month < 1 or year < 1:
-        return False
-    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        Leap = True
-    if month < 1 or month > 12:
-        return False
-    if day < 1 or day > 31:
-        return False
-    else:
-        if month % 2 == 0:
-            if (day <= 29 and Leap == True and month == 2) or (day <= 30 and month != 2):
-                return True
-            else:
+    try:
+        day, month, year = map(int, string.split('.'))
+        if month < 1 or month > 12 or day < 1 or day > 31:
+            return False
+
+        if month in [4, 6, 9, 11] and day > 30:
+            return False
+
+        if month == 2:
+            leap = (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+            if day > 29 or (day == 29 and not leap):
                 return False
-        else:
-            if (day <= 31):
-                return True
-            else:
-                return False
+
+        return True
+    except:
+        return False
 
 def find_in_text(string : str) -> list:
-    list_of_dates = findall(DATE_FORMAT, string)
-    new_list = []
-    for i in list_of_dates:
-        if isCorrect(i):
-            new_list.append(i)
-
-    return new_list
+    list_of_dates = DATE_FORMAT.findall(string)
+    return [date for date in list_of_dates if isCorrect(date)]
 
 def find_in_file(file_name : str) -> list:
     new_list = []
-
     for encoding in ('utf-8', 'cp1251'):
         try:
             with open(file_name, 'r', encoding=encoding) as file:
                 for line in file:
-                    matches = findall(DATE_FORMAT, line)
+                    matches = DATE_FORMAT.findall(line)
                     for match in matches:
                         if isCorrect(match):
                             new_list.append(match)
@@ -62,14 +49,14 @@ def find_in_web(url_name: str) -> list:
         response.raise_for_status()
 
         content = ""
-        for encoding in ('utf-8', 'latin1'):
+        for encoding in ('utf-8', 'cp1251'):
             try:
                 content = response.content.decode(encoding)
                 break
             except UnicodeDecodeError:
                 continue
 
-        matches = findall(DATE_FORMAT, content)
+        matches = DATE_FORMAT.findall(content)
         for match in matches:
             if isCorrect(match):
                 new_list.append(match)
@@ -79,4 +66,4 @@ def find_in_web(url_name: str) -> list:
         print("Ошибка при подключении к URL: ", e)
         return []
 
-print(find_in_web(""))
+print(find_in_web(GIST_URL))
